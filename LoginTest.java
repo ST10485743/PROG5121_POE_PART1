d4997c5f323de43e8fe63cf21df289a195eabfe4
ST10485743 <ST10485743@rcconnect.edu.za>
2026-09-16 13:05:52 +0200

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ *
+ * @author leago
+ */
+class LoginTest {
+    
+    private Login login;
+    
+    @BeforeEach
+    void setUp() {
+        login = new Login();
+    }
+    
+    //------------- assertTrue/assertFalse Tests ------------
+    
+    @Test
+     void checkUserName_CorrectlyFormatted_ReturnsTrue() {
+         assertTrue(login.checkUserName("kyl_1"));
+     }
+     
+     @Test
+     void checkUserName_IncorrectlyFormatted_ReturnsFalse(){
+         assertFalse(login.checkUserName("kyle!!!!!!"));
+     }
+     
+     @Test
+     void checkPasswordComplexity_MeetsRequirements_ReturnsTrue() {
+         assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+     }
+    
+     @Test
+     void checkPasswordComplexity_DoesNotMeetRequirements_ReturnsFalse() {
+         assertFalse(login.checkPasswordComplexity("password"));
+     }
+     
+     @Test
+     void checkCellPhoneNumber_CorrectlyFormatted_ReturnsTrue(){
+         assertTrue(login.checkCellPhoneNumber("+27838968976"));
+     }
+     
+     @Test
+     void checkCellPhoneNumber_IncorrectlyFormatted_ReturnsFalse(){
+         assertFalse(login.checkCellPhoneNumber("08966553"));
+     }
+     
+     @Test
+     void loginUser_CorrectCredentials_ReturnsTrue() {
+         login.registerUser("Kyle", "Peters", "kyl_1", "Ch&&sec@ke99!", "+27838968976");
+         assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
+     }
+     
+     @Test
+     void loginUser_IncorrectCredentials_ReturnsFalse(){
+         login.registerUser("Kyle", "Peters", "kyl_1", "Ch&&sec@ke99!", "+27838968976");
+         assertFalse(login.loginUser("kyl_1", "wrongPassword1!"));
+     }
+     
+     // ---------- assertEquals tests (registerUser messages) ----------
+     
+     @Test
+     void registerUser_UsernameIncorrectlyFormatted_ReturnsUsernameMessage() {
+         String result = login.registerUser("Kyle", "Peters", "kyle!!!!!!",
+                "Ch&&sec@ke99!", "+27838968976");
+         assertEquals("Username is not correctly formatted; please ensure that "
+                + "your username contains an underscore and is no more than "
+                + "five characters in length.", result);
+     }
+     
+     @Test
+     void registerUser_PasswordDoesNotMeetComplexity_ReturnsPasswordMessage() {
+         String result = login.registerUser("Kyle", "Peters", "kyl_1",
+                "password", "+27838968976");
+         assertEquals("Password is not correctly formatted; please ensure that "
+                + "the password contains at least eight characters, a capital "
+                + "letter, a number, and a special character.", result);
+     }
+     
+    
+}
